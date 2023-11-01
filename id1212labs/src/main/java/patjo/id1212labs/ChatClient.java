17e0ca01f7570f7b4485fbd77be590b19e2e911a
wPatr <91604767+wPatr@users.noreply.github.com>
2023-11-01 16:04:32 +0100

    create classes

---
+++ id1212labs/src/main/java/patjo/id1212labs/ChatClient.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package patjo.id1212labs;
+
+/**
+ *
+ * @author patricialagerhult
+ */
+public class ChatClient {
+    
+}
--- id1212labs/src/main/java/patjo/id1212labs/Main.java
+++ id1212labs/src/main/java/patjo/id1212labs/Main.java
@@ -5,7 +5,7 @@ package patjo.id1212labs;
  *
  * @author PC
  */
-public class Id1212labs {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
