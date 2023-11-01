292761a674c53bb000b99b797329b765a319eaa2
Noxiium <johansf98@hotmail.com>
2023-11-01 15:41:26 +0100

    first commit of project

---
+++ id1212labs/src/main/java/patjo/id1212labs/Id1212labs.java
@@ -0,0 +1,13 @@
+
+package patjo.id1212labs;
+
+/**
+ *
+ * @author PC
+ */
+public class Id1212labs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
