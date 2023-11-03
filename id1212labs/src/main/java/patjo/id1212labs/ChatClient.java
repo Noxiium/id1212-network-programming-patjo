1076701ac2ff58550ece5428e0b2f757138e167c
wPatr <91604767+wPatr@users.noreply.github.com>
2023-11-03 09:38:37 +0100

    Added threads and username to client, removed printlines
    Co-Authored-By: Johan Seller Fredlund <90914884+Noxiium@users.noreply.github.com>

--- id1212labs/src/main/java/patjo/id1212labs/ChatClient.java
+++ id1212labs/src/main/java/patjo/id1212labs/ChatClient.java
@@ -12,70 +12,66 @@ import java.util.Scanner;
  * @author patricialagerhult & johansellerfredlund
  */
 public class ChatClient extends Thread {
+
+    int serverPort = 5555;
     Socket socket;
     BufferedReader br;
     BufferedWriter bw;
     InputStreamReader inputStreamReader;
     OutputStreamWriter outputStreamWriter;
-    
-    public ChatClient(){
+
+    public ChatClient() {
         try {
-            this.socket = new Socket("localhost",5555);
+            this.socket = new Socket("localhost", serverPort);
             this.inputStreamReader = new InputStreamReader(socket.getInputStream());
             this.br = new BufferedReader(inputStreamReader);
             this.outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
             this.bw = new BufferedWriter(outputStreamWriter);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
-    //sendMessage
-    @Override
-      public void run(){
-          try {
-              System.out.println("Write a message:");
-              Scanner scanner = new Scanner(System.in);
-              
-              
-              while(socket.isConnected()){
+ 
+    public void writeMessage() {
+        try {
+            System.out.println("Write a message:");
+            Scanner scanner = new Scanner(System.in);
+
+            while (socket.isConnected()) {
                 String msg = scanner.nextLine();
-                bw.write(msg);
+                bw.write(socket.getLocalPort() + ":" + msg);
                 bw.newLine();
                 bw.flush();
-                System.out.println("jjjj");
-              }
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
-        
-        
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-      
-      public void listenForMessage(){
-            
-          try {    
-              while(socket.isConnected()){
-                  System.out.println("Listening..");
-                 String receivedMsg = br.readLine();
-                  System.out.println(receivedMsg);
-                   System.out.println("DONE");
-              }
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
+
+    public void listenForMessage() {
+
+        try {
+            while (socket.isConnected()) {
+                String receivedMsg = br.readLine();
+                System.out.println(receivedMsg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-      
-   
+
     public static void main(String[] args) {
+
         ChatClient chatClient = new ChatClient();
-        chatClient.start();
-        chatClient.listenForMessage();
-        
-        
-}
-  
-}
 
+        Thread thread1 = new Thread(() -> chatClient.writeMessage());
+        Thread thread2 = new Thread(() -> chatClient.listenForMessage());
 
+        thread1.start();
+        thread2.start();
+
+    }
+
+}
