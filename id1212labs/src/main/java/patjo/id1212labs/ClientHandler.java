fa8daf71c79809fdbbbc56cb68ee486a52ad5b7c
wPatr <91604767+wPatr@users.noreply.github.com>
2023-11-03 09:39:46 +0100

    ClientHandler class
    Co-Authored-By: Johan Seller Fredlund <90914884+Noxiium@users.noreply.github.com>

--- id1212labs/src/main/java/patjo/id1212labs/ClientHandler.java
+++ id1212labs/src/main/java/patjo/id1212labs/ClientHandler.java
@@ -1,24 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package patjo.id1212labs;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  *
- * @author patricialagerhult
+ * @author patricialagerhult & johansellerfredlund
  */
 public class ClientHandler implements Runnable {
-    
-    
-    public ClientHandler(Socket clientSocket){
-    
+
+    Socket clientSocket;
+    BufferedReader br;
+    BufferedWriter bw;
+    InputStreamReader inputStreamReader;
+    OutputStreamWriter outputStreamWriter;
+    static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+    public ClientHandler(Socket clientSocket) {
+        try {
+            System.out.println(clientSocket);
+            this.clientSocket = clientSocket;
+            this.inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+            this.br = new BufferedReader(inputStreamReader);
+            this.outputStreamWriter = new OutputStreamWriter(clientSocket.getOutputStream());
+            this.bw = new BufferedWriter(outputStreamWriter);
+            clientHandlers.add(this);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
+        while (clientSocket.isConnected()) {
+            try {
+                String msg = br.readLine();
+                sendToAll(msg);
+            } catch (Exception e) {
+                closeAll(clientSocket, br, bw);
+            }
+        }
     }
-    
+
+    public void sendToAll(String msg) {
+        for (ClientHandler client : clientHandlers) {
+            try {
+                client.bw.write(msg);
+                client.bw.newLine();
+                client.bw.flush();
+
+            } catch (Exception e) {
+                closeAll(clientSocket, br, bw);
+            }
+        }
+    }
+
+    public void removeClientFromClientHandlers() {
+        clientHandlers.remove(this);
+
+    }
+
+    public void closeAll(Socket socket, BufferedReader br, BufferedWriter bw) {
+        removeClientFromClientHandlers();
+        try {
+            clientSocket.close();
+            br.close();
+            bw.close();
+
+        } catch (Exception e) {
+            closeAll(clientSocket, br, bw);
+        }
+    }
 }
