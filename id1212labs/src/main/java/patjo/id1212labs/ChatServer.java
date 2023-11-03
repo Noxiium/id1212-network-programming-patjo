6b4a40db299f5ff75ce6f6e92bfa2c2dd7fd94c1
Noxiium <johansf98@hotmail.com>
2023-11-03 09:48:54 +0100

    Fixed merge errors, working error handling

--- id1212labs/src/main/java/patjo/id1212labs/ChatServer.java
+++ id1212labs/src/main/java/patjo/id1212labs/ChatServer.java
@@ -9,10 +9,12 @@ import java.net.Socket;
  * @author patricialagerhult & johansellerfredlund
  */
 public class ChatServer {
-    
+    ServerSocket serverSocket;
+    int serverPort = 5555;
+
     public ChatServer() {
         try {
-            serverSocket = new ServerSocket(5555);
+            serverSocket = new ServerSocket(serverPort);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,34 +39,4 @@ public class ChatServer {
         }
     }
 
-}
-    public ChatServer(){
-        try {
-            serverSocket = new ServerSocket(5555);
-            System.out.println("Server listen on port 5555 ...");
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        
-    }
-     
-    public void listenForClient(){
-    
-        while(true){
-            try {
-                Socket socket = serverSocket.accept(); 
-                System.out.println("New client connected " + socket + " ");
-                ClientHandler clientHandler = new ClientHandler(socket);
-        
-                Thread thread = new Thread(clientHandler);
-                
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        
-        }
-    }
-    
 }
