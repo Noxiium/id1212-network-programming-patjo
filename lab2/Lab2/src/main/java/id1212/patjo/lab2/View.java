413198a06b86ab27cc8516911a5e7e611a6fb97d
Noxiium <johansf98@hotmail.com>
2023-11-13 15:33:54 +0100

    we can now connect

--- lab2/Lab2/src/main/java/id1212/patjo/lab2/View.java
+++ lab2/Lab2/src/main/java/id1212/patjo/lab2/View.java
@@ -2,6 +2,8 @@ package id1212.patjo.lab2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
@@ -15,26 +17,41 @@ public class View {
     BufferedReader request;
     InputStreamReader inputStreamReader;
     PrintStream response;
+    String folder = "C:\\Users\\PC\\git_projects\\id1212-network-programming-patjo\\lab2\\Lab2\\src\\main\\java\\id1212\\patjo\\lab2\\";
 
     public View(Socket socket) {
-        
+
         try {
-            
-            this. inputStreamReader = new InputStreamReader(socket.getInputStream());
+
+            this.inputStreamReader = new InputStreamReader(socket.getInputStream());
             this.request = new BufferedReader(inputStreamReader);
             this.response = new PrintStream(socket.getOutputStream());
-            String str = request.readLine();
-            while ((str = request.readLine()) != null && str.length() > 0) {
-            System.out.println(str);
-        }
         } catch (Exception e) {
-                e.printStackTrace();
+            e.printStackTrace();
         }
-        
-        
+
     }
-    
-    public String generateHTTPResponse(Boolean answer, int numberOfGuessesLeft){
-       return null; 
+
+    private void initialReponse() {
+        response.println("HTTP/1.1 200 OK");
+        response.println("Server: Patjo 0.1 Beta");
+        response.println("Content-Type: text/html");
+        response.println("Set-Cookie: baam=1337; expires=Wednesday,31-Dec-23 21:00:00 GMT");
+        response.println();
+        String requestedDocument = folder + "index.html";
+        try {
+            File f = new File("" + requestedDocument);
+            FileInputStream infil = new FileInputStream(f);
+            byte[] b = new byte[1024];
+            while (infil.available() > 0) {
+                response.write(b, 0, infil.read(b));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String generateHTTPResponse(Boolean answer, int numberOfGuessesLeft) {
+        return null;
     }
 }
