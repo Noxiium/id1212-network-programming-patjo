cf3062e650990f06cccd4b27a6f16574edeb894c
wPatr <91604767+wPatr@users.noreply.github.com>
2023-11-21 10:34:14 +0100

    initial commit

---
+++ Lab3/src/main/java/id1212/patjo/lab3/Lab3.java
@@ -0,0 +1,13 @@
+
+package id1212.patjo.lab3;
+
+/**
+ *
+ * @author patricialagerhult
+ */
+public class Lab3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
