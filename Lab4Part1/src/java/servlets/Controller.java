3380f0d66cd3c3979b84ebf44f58675e42d9d6dd
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-01 14:34:07 +0100

    add userinput into database

--- Lab4Part1/src/java/servlets/Controller.java
+++ Lab4Part1/src/java/servlets/Controller.java
@@ -1,4 +1,3 @@
-
 package servlets;
 
 import java.io.IOException;
@@ -8,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.*;
 
 @WebServlet(name = "Controller", urlPatterns = {"/Controller"})
 public class Controller extends HttpServlet {
@@ -19,7 +19,6 @@ public class Controller extends HttpServlet {
         //Get or create a session for the current client 
         HttpSession session = request.getSession(true);
 
-
         //Retrieve the model associated with the current session,
         //or create a new one if none existed.
         /* Model model = (Model) session.getAttribute("model");
@@ -27,7 +26,7 @@ public class Controller extends HttpServlet {
             model = new Model();
             session.setAttribute("model", model);
         }
-        */
+         */
         //Forward request to index.jsp
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
@@ -36,9 +35,33 @@ public class Controller extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String userInput = request.getParameter("guess");
+        request.setAttribute("guess", userInput);
 
-        String userInput = request.getParameter("userGuess");
-        request.setAttribute("userGuess", userInput);
+        
+
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conn = (Connection) DriverManager.getConnection("jdbc:derby://localhost:1527/Derby", "patjo", "patjo");
+
+            //String query = "INSERT INTO APP.USERS(USERNAME, PASSWORD) VALUES (?,?)";
+
+            //PreparedStatement preparedStatement = conn.prepareStatement(query);
+            //preparedStatement.setString(1, "hej");    // NAME
+            //preparedStatement.setString(2, "password"); // PASSWORD
+            //preparedStatement.executeUpdate();
+       
+
+            //preparedStatement.close();
+            
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO APP.USERS (USERNAME, PASSWORD) VALUES('"+ userInput +"', 'password')");
+            //statement.executeUpdate("INSERT INTO APP.USERS (USERNAME, PASSWORD) VALUES('"+userInput+"', 'test')");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("hello");
+        }
         request.getRequestDispatcher("index.jsp").forward(request, response);
 
     }
