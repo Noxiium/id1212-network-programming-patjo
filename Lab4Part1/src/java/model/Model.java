34908979ed4ececc88b1995cfb805a6ea0933c21
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-01 11:31:03 +0100

    added model

---
+++ Lab4Part1/src/java/model/Model.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.util.Random;
+
+/**
+ * GameModel represents an instance of a game session. Holds information about
+ * the current game.
+ *
+ * @author patricialagerhult && johansellerfredlund
+ */
+public class Model {
+
+    private final int NUMBER_OF_GUESSES_START_VALUE = 0;
+    private int randomNumber;
+    private int numberOfGuesses;
+
+    /**
+     * Creates a new instance of gameModel 
+     * Generates a random number, and NumberOfGuesses is set to 0
+     */
+    public Model() {
+        generateRandomNumber();
+        this.numberOfGuesses = NUMBER_OF_GUESSES_START_VALUE;
+    }
+
+    private void generateRandomNumber() {
+        Random rand = new Random();
+        this.randomNumber = rand.nextInt(100) + 1;
+
+    }
+
+    //Return the random number.
+    public int getRandomNumer() {
+        return this.randomNumber;
+    }
+
+    public int getNumberOfGuesses() {
+        return this.numberOfGuesses;
+    }
+
+    public void restart() {
+        resetNumberOfGuesses();
+        generateRandomNumber();
+    }
+
+    private void resetNumberOfGuesses() {
+        this.numberOfGuesses = NUMBER_OF_GUESSES_START_VALUE;
+    }
+
+    public boolean checkAnswer(int userInput) {
+        this.numberOfGuesses++;
+        return userInput == randomNumber;
+    }
+}
