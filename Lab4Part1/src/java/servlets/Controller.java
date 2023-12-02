e6638696d3fea5f36fcbf21d2e32f44984fc8c21
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-02 01:53:03 +0100

    created method for retrieving sessionModel

--- Lab4Part1/src/java/servlets/Controller.java
+++ Lab4Part1/src/java/servlets/Controller.java
@@ -17,22 +17,10 @@ public class Controller extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        System.out.println("doGet");
-        
-        
-        //Get or create a session for the current client 
-        HttpSession session = request.getSession(true);
+        // Retrieve the model associated with the current session,
+        // or a new one if none existed.
+        Model model = getOrCreateSessionModel(request);
 
-        //Retrieve the model associated with the current session,
-        //or create a new one if none existed.
-        Model model = (Model) session.getAttribute("model");
-        if (model == null) {
-            model = new Model();
-            session.setAttribute("model", model);
-        }
-
-        
-        
         //Forward request to index.jsp
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
@@ -41,31 +29,45 @@ public class Controller extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        // Retrieve the model associated with the current session,
+        // or a new one if none existed.
+        Model model = getOrCreateSessionModel(request);
+        System.out.println(model.getRandomNumer());
+
+        String userInput = request.getParameter("guess");
+        request.setAttribute("guess", userInput);
+
+        boolean isCorrectAnswer = model.checkAnswer(Integer.parseInt(userInput));
+
+        // Retrieve the number of guesses made from the Model associated with the current session,
+        // and set it as an attribute in the request for display in the JSP.
+        int numberOfGuessesMade = model.getNumberOfGuesses();
+        request.setAttribute("numberOfGuessesMade", numberOfGuessesMade);
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+    }
+
+    /**
+     * Returns the model associated with the current session, or create a new
+     * one if none existed.
+     *
+     * @param request
+     * @return model
+     */
+    private Model getOrCreateSessionModel(HttpServletRequest request) {
+
+        // Get or create a session for the current client 
         HttpSession session = request.getSession(true);
 
+        // Retrieve the model associated with the current session,
+        // or create a new one if none existed.
         Model model = (Model) session.getAttribute("model");
         if (model == null) {
             model = new Model();
             session.setAttribute("model", model);
         }
-        System.out.println(model.getRandomNumer());
-        
-        String userInput = request.getParameter("guess");
-        request.setAttribute("guess", userInput);
-        
-        
-        boolean isCorrectAnswer = model.checkAnswer(Integer.parseInt(userInput));
-        
-        // Retrieve the number of guesses made from the model associated with the current session,
-        // and set it as an attribute in the request for display in the JSP.
-        int numberOfGuessesMade = model.getNumberOfGuesses();
-        request.setAttribute("numberOfGuessesMade", numberOfGuessesMade);
-        
-        
-        
-        
-        request.getRequestDispatcher("index.jsp").forward(request, response);
-
+        return model;
     }
 }
 
