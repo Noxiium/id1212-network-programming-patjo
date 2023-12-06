fc46173b164cdf2d3e02dfe2b2e3ea721b14d675
Noxiium <johansf98@hotmail.com>
2023-12-06 11:34:20 +0100

    Created GameSessionModel

---
+++ Lab4Part2/src/java/model/GameSessionModel.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+
+
+
+public class GameSessionModel{
+        int quizID;
+        int totalScore;
+        ArrayList<Integer> questionsID;
+
+    public GameSessionModel(){
+
+
+    }
+    
+
+    public void getNumberOfQuestionsFromDB(int quizID){
+        
+    }
+
+}
