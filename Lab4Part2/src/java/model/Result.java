b883a4da0071ffb544ded3fc6fcfe5888a334235
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-07 23:21:29 +0100

    added entity for JPA

--- Lab4Part2/src/java/model/Result.java
+++ Lab4Part2/src/java/model/Result.java
@@ -1,41 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-import javax.persistence.*;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "RESULTS")
-public class Result {
+@Table(name = "APP.RESULTS")
+public class Result implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long ID;
+    @Column(name = "ID")
+    private Integer id;
 
     @Column(name = "USER_ID")
-    private int USER_ID;
-    
+    private Integer userId;
+
     @Column(name = "QUIZ_ID")
-    private int QUIZ_ID;
-     
-    private int SCORE;
+    private Integer quizId;
+
+    @Column(name = "SCORE")
+    private Integer score;
+
+    public Result() {
+        // Default constructor needed by JPA
+    }
+
     
-    public Result(){
-      
-    }
-    
-    public void setUserId(int userId) {
-        this.USER_ID = userId;
+    public Integer getId() {
+        return id;
     }
 
-    public void setQuizId(int quizId) {
-        this.QUIZ_ID = quizId;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public void setScore(int score) {
-        this.SCORE = score;
+    public Integer getUserId() {
+        return userId;
     }
 
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(Integer quizId) {
+        this.quizId = quizId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Result)) {
+            return false;
+        }
+        Result other = (Result) object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString() {
+        return "model.Result[ id=" + id + " ]";
+    }
 }
