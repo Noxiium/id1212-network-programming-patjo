0388ebf9ae40069af3457f64d41cf15e2bc35b8f
Noxiium <johansf98@hotmail.com>
2023-12-07 12:13:56 +0100

    added resultModel to fectch results from DB

--- Lab4Part2/src/java/model/resultModel.java
+++ Lab4Part2/src/java/model/resultModel.java
@@ -4,10 +4,74 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author PC
  */
-public class resultModel {
+public class ResultModel {
+    public ArrayList<ResultTableDTO> resultList;
     
+    public ResultModel() {
+        this.resultList = new ArrayList<ResultTableDTO>();
+    }
+
+    public void fetchResultsFromDB() throws SQLException {
+        String userName;
+        String quizSubject;
+        String score;
+        Statement statement = connectToDB();
+        ResultSet queryResult = statement.executeQuery("SELECT USER_ID, QUIZ_ID, SCORE FROM APP.RESULT");
+
+        while (queryResult.next()) {
+            String user_id = queryResult.getString("USER_ID");
+            String quiz_id = queryResult.getString("QUIZ_ID");
+            score = queryResult.getString("SCORE");
+            userName = fetchUserNameFromDB(user_id);
+            quizSubject = fetchQuizSubjectTextFromDB(quiz_id);
+            ResultTableDTO result = new ResultTableDTO(userName, quizSubject, score);
+            resultList.add(result);
+        }
+    }
+
+    private String fetchQuizSubjectTextFromDB(String quiz_id) throws SQLException {
+        Statement statement = connectToDB();
+        ResultSet queryResult = statement.executeQuery("SELECT SUBJECT FROM APP.QUIZZES WHERE ID =" + quiz_id);
+        while (queryResult.next()) {
+            String quizSubject = queryResult.getString("SUBJECT");
+            return quizSubject;
+        }
+        return "N/A";
+    }
+
+    private String fetchUserNameFromDB(String user_id) throws SQLException {
+        Statement statement = connectToDB();
+        ResultSet queryResult = statement.executeQuery("SELECT USERNAME FROM APP.USERS WHERE ID =" + user_id);
+        while (queryResult.next()){
+            String userName = queryResult.getString("USERNAME");
+            return userName;
+        }
+        return "N/A";
+    }
+
+    private Statement connectToDB() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conn = (Connection) DriverManager.getConnection("jdbc:derby://localhost:1527/Derby", "patjo",
+                    "patjo");
+            Statement statement = conn.createStatement();
+
+            return statement;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
