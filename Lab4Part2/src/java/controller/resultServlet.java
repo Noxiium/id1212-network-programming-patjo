2c86dca3c324d74ea5737ea599cfe6ada3bad37d
Noxiium <johansf98@hotmail.com>
2023-12-07 12:14:42 +0100

    resultServlet?

--- Lab4Part2/src/java/controller/resultServlet.java
+++ Lab4Part2/src/java/controller/resultServlet.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.ResultModel;
 
 /**
  *
@@ -28,58 +25,36 @@ public class ResultServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet resultServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet resultServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            request.getRequestDispatcher("loginView.jsp").forward(request, response);
+
+        } else {
+            
+            ResultModel model = getOrCreateSessionModel(request);
+            ArrayList<Object> historyResult = model.fetchResultsFromDB();
+           
+            request.setAttribute("list", historyResult);
+            
+            request.getRequestDispatcher("historyResultView.jsp").forward(request, response);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    
+     private ResultModel getOrCreateSessionModel(HttpServletRequest request) {
+
+        // Get or create a session for the current client 
+        HttpSession session = request.getSession(true);
+
+        // Retrieve the model associated with the current session,
+        ResultModel model = (ResultModel) session.getAttribute("resultModel");
+        if (model == null) {
+            model = new ResultModel();
+            session.setAttribute("resultModel", model);
+        }
+        return model;
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
