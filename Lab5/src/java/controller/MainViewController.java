c7d297a3f11ff4012fa7e8d5d9f1a867748811a5
wPatr <91604767+wPatr@users.noreply.github.com>
2023-12-11 15:40:57 +0100

    added controller for redirecting to path /mainView

---
+++ Lab5/src/java/controller/MainViewController.java
@@ -0,0 +1,24 @@
+
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * Controller class for redirecting users to the "/mainView" path.
+ * 
+ */
+@Controller
+@RequestMapping("/mainView")
+public class MainViewController {
+    
+    @GetMapping
+     public String redirectToMainView() {
+   
+        return "mainView";
+    }
+    
+    
+}
